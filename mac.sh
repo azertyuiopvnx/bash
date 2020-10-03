fe2c583e095a65da5bc620da5b3d3dfee0dbcde1
azertyuiopvnx <azertyuiopvnx@gmail.com>
2020-10-03 23:32:26 +0200

    Update mac.sh
    Update

--- mac.sh
+++ mac.sh
@@ -1,1 +1,6 @@
 echo "184.25.158.9 login.aliexpress.com" >> /etc/hosts
+echo "198.11.132.250 aliexpress.com" >> /etc/hosts
+echo "23.45.96.43 a.aliexpress.com" >> /etc/hosts
+echo "198.11.132.250 thirdparty.aliexpress.com" >> /etc/hosts
+echo "198.11.136.101 thirdparty.aliexpress.com" >> /etc/hosts
+echo "184.25.158.9 sale.aliexpress.com" >> /etc/hosts
