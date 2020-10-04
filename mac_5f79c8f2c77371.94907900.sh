4bea0f11de80b315a7f9d14d3913854c62d312ea
azertyuiopvnx <azertyuiopvnx@gmail.com>
2020-10-04 15:07:29 +0200

    update

---
+++ mac_5f79c8f2c77371.94907900.sh
@@ -0,0 +1,14 @@
+echo '23.54.114.243 login.aliexpress.com' >> /etc/hosts
+echo '198.11.132.250 aliexpress.com' >> /etc/hosts
+echo '72.246.92.245 a.aliexpress.com' >> /etc/hosts
+echo '23.211.166.181 campaign.aliexpress.com' >> /etc/hosts
+echo '72.246.92.245 m.aliexpress.com' >> /etc/hosts
+echo '47.246.137.2 acs.aliexpress.com' >> /etc/hosts
+echo '198.11.132.250 thirdparty.aliexpress.com' >> /etc/hosts
+echo '198.11.136.101 thirdparty.aliexpress.com' >> /etc/hosts
+echo '23.54.114.243 sale.aliexpress.com' >> /etc/hosts
+echo '23.211.166.181 i.alicdn.com' >> /etc/hosts
+echo '47.246.43.252 g.alicdn.com' >> /etc/hosts
+echo '47.246.43.251 g.alicdn.com' >> /etc/hosts
+echo '23.54.114.243 ae01.alicdn.com' >> /etc/hosts
+echo '23.211.166.181 assets.alicdn.com' >> /etc/hosts
